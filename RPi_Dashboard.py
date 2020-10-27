b51125eed3d7e0ae06d15908565368ee543677eb
Jack Yang <yang.jubiao.rpi@gmail.com>
2020-10-26 23:41:31 -0400

    further improvement w sunrise/sunset time display; add snapshot image

--- RPi_Dashboard.py
+++ RPi_Dashboard.py
@@ -5,10 +5,13 @@ import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+import matplotlib.patheffects as PathEffects
 import cartopy.crs as ccrs
 from cartopy.feature.nightshade import Nightshade
 import json
 import urllib3
+from suntime import Sun
+import math
 
 cpuTimes = psutil.cpu_times()
 cpuCount = psutil.cpu_count()
@@ -73,11 +76,11 @@ class updateAxes:
         cpuPercent = extractSysStats(self.sysStats, 'cpuPercent')
         #CPU temperature
         cpuTempCel = extractSysStats(self.sysStats, 'cpuTempCel')
-        line, = self.ax2.plot(sysStatsX, cpuTempCel, 'd-r', ms=3)
+        line, = self.ax2.plot(sysStatsX, cpuTempCel, '2:r', ms=3, lw=.5)
         self.lines[1]['cpuTempCel'] = line
         #memory percertange
         memPercent = extractSysStats(self.sysStats, 'memPercent')
-        line, = self.ax2.plot(sysStatsX, memPercent, 'o-c', ms=3)
+        line, = self.ax2.plot(sysStatsX, memPercent, '.-c', ms=3, lw=.5)
         self.lines[1]['memPercent'] = line
         #globe
 #         self.ax4.stock_img()
@@ -110,12 +113,36 @@ class updateAxes:
             self.ax4.coastlines()
 #             self.ax4.stock_img()
 #             self.ax4.background_img(name='BM', resolution='low')
-            self.ax4.add_feature(Nightshade(datetime.utcnow(), alpha=.3))
-            scatterLongitudes = [x[1] for x in CitiesCoords.values()]+[geolocCoords[1]]
-            scatterLatitudes = [x[0] for x in CitiesCoords.values()]+[geolocCoords[0]]
-            scatterColors = ['y']*len(CitiesCoords)+['r']
-            self.ax4.scatter(scatterLongitudes, scatterLatitudes, s=30, c=scatterColors, alpha=.8, transform=ccrs.PlateCarree())
+            utcnow = datetime.utcnow()
+            self.ax4.add_feature(Nightshade(utcnow, alpha=.15))
+            scatterLongitudes = [x[1] for x in CitiesCoords.values()]#+[geolocCoords[1]]
+            scatterLatitudes = [x[0] for x in CitiesCoords.values()]#+[geolocCoords[0]]
+            scatterColors = ['y']*len(CitiesCoords)#+['r']
+            self.ax4.scatter(scatterLongitudes, scatterLatitudes, s=15, c=scatterColors, alpha=.8, transform=ccrs.PlateCarree())
+            self.ax4.plot([geolocCoords[1]], [geolocCoords[0]], ms=5, c='r', transform=ccrs.PlateCarree(),
+                          **{'marker': '$\\bigoplus$', 'linestyle': '', 'markeredgewidth': .05})
             self.ax4.gridlines(crs=ccrs.PlateCarree(), xlocs=[], ylocs=[geolocCoords[0]], color='r')
+            for (k,v) in CitiesCoords.items():
+                self.ax4.text(v[1]+3, v[0]-3, k, fontsize='xx-small', color='b', alpha=.95, horizontalalignment='left',
+                              verticalalignment='top', transform=ccrs.Geodetic())
+            sun = Sun(geolocCoords[0], geolocCoords[1])
+            today = utcnow.date()
+            sunrises = [sun.get_sunrise_time(d) for d in [today-timedelta(days=1), today, today+timedelta(days=1)]]
+            sunsets = [sun.get_sunset_time(d) for d in [today-timedelta(days=1), today, today+timedelta(days=1)]]
+            sunTimes = sunrises+sunsets
+            sunTimes = [d.replace(tzinfo=None) for d in sunTimes]
+            sunTimes.sort()
+            sunTimeLast = [t for t in sunTimes if t<utcnow.replace(tzinfo=None)][-1]
+            sunTimeNext = [t for t in sunTimes if t>=utcnow.replace(tzinfo=None)][0]
+            sunHoursDeltas = [t-utcnow for t in [sunTimeLast, sunTimeNext]]
+            sunHours = [td.days*24+td.seconds/3600 for td in sunHoursDeltas]
+            sunHHMMs = ['{:+03.0f}:{:02.0f}'.format(math.trunc(sh), 60*abs(sh-math.trunc(sh))) for sh in sunHours]
+            meStrTxts = [
+                self.ax4.text(geolocCoords[1]+txtPosShiftHor, geolocCoords[0]+3, meStr, fontsize='xx-small', fontweight='normal', color='m',
+                                     alpha=.99, horizontalalignment=txtPosAlignHor, transform=ccrs.Geodetic())
+                for (meStr, txtPosAlignHor, txtPosShiftHor) in zip(sunHHMMs, ['right', 'left'], [-3, 3])
+            ]
+            for txtObj in meStrTxts: txtObj.set_path_effects([PathEffects.withStroke(linewidth=3, foreground='w')])
             self.ax4TimeToUpdate += timedelta(seconds=21)
         
 
